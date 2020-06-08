8bb11a2261c2ba44265ec9191ebb046f1aa53502
Luke DeRienzo <javasoftwarearchitect@gmail.com>
2020-06-08 15:37:08 -0400

    addition of mongodb and CartItem model along with code to save item to db to be displayed for shopping cart.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Required External Modules
  */
-
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 
@@ -12,14 +12,27 @@ const path = require("path");
 /**
  *  App Configuration
  */
-
+// create app and expose port on which app will run
 const app = express();
 const port = process.env.PORT || "8000";
 
+// set path to view templates and set template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
+
+// load CartItem model
+const CartItem = require("./models/CartItem");
+
+// DB Config
+const url = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(url,{ useNewUrlParser: true , useUnifiedTopology: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 /**
  * Routes Definitions
@@ -31,6 +44,30 @@ app.get("/", (req, res) => {
 
 app.get("/shopping_cart", (req, res) => {
   console.log(req.query)
+  if (req.query.supply && req.query.price) {
+  	// query if item is already present if so, update by incrementing quantity by one and adding to price extended
+  	CartItem.findOne({ supplyAmount: req.query.supply }).then(cartItem =>{
+  		if (cartItem) {
+  			cartItem.quantity++;
+  			cartItem.extended += Number(req.query.price); 
+  			cartItem
+			  .save()
+			  .catch(err => console.log(err));
+  		}
+  		else {			
+		  	const newCartItem = new CartItem({
+		  		supplyAmount: req.query.supply,
+		        itemDescription: req.query.supply + " Month Supply of AlgaeCal Plus & Strontium Boost",
+		        price: req.query.price,
+		        quantity: 1,
+		        extended: req.query.price
+		      });
+		  	newCartItem
+			  .save()
+			  .catch(err => console.log(err));
+  		}
+  	})
+  }
   res.render("shopping_cart", { title: "Bundle Added", bundleAdded: { month_supply: req.query.supply, price:req.query.price } });
 });
 
---
+++ models/CartItem.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const CartItemSchema = new Schema({
+  supplyAmount: {
+    type: Number,
+    required: true
+  },
+  itemDescription: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  extended: {
+    type: Number,
+    required: true 
+  }
+});
+
+module.exports = CartItem = mongoose.model("CartItem", CartItemSchema);
